b4fda80b8a9d1c2972ee8c86ba952f1290a66a41
Vinícius Cantuária <cantuariavc@gmail.com>
2021-05-24 12:10:51 -0300

    Atualiza conexão com a base de dados com ConnectionFactory

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,14 +1,11 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Principal {
 
 	public static void main(String[] args) throws SQLException {
-		Connection connection = DriverManager.getConnection(
-				"jdbc:mysql://localhost/faculdadeHepta",
-				"root",
-				"root");
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.recuperaConexao();
 		System.out.println("Conectado com a base de dados.");
 		
 		connection.close();
