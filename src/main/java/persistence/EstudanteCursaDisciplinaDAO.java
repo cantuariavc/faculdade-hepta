eb44ac85b96a47caf1f0f23ebbfb8e25439fae3e
Vinícius Cantuária <cantuariavc@gmail.com>
2021-05-25 09:17:23 -0300

    Adiciona listagem de estudantes que cursam determinada disciplina

--- src/main/java/persistence/EstudanteCursaDisciplinaDAO.java
+++ src/main/java/persistence/EstudanteCursaDisciplinaDAO.java
@@ -2,10 +2,15 @@ package persistence;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.Disciplina;
 import model.Estudante;
+import model.EstudanteCursaDisciplina;
+import model.Professor;
 
 public class EstudanteCursaDisciplinaDAO {
 
@@ -24,6 +29,27 @@ public class EstudanteCursaDisciplinaDAO {
 			
 			pstm.execute();
 		}
+	}
+	
+	public List<Estudante> listarEstudantes(int idDisciplina) throws SQLException {
+		List<Estudante> estudantes = new ArrayList<Estudante>();
+		String sql = "SELECT e.idEstudante, e.nomeCompleto "
+					+ "FROM cursa c INNER JOIN ESTUDANTE e ON e.idEstudante = c.idEstudante "
+					+ "WHERE c.idDisciplina = "+idDisciplina+"";
+		
+		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+			pstm.execute();
+
+			try (ResultSet rst = pstm.getResultSet()) {
+				Estudante estudante;
+				while (rst.next()) {
+					estudante = new Estudante(rst.getInt("idEstudante"), rst.getString("nomeCompleto"));
+					estudantes.add(estudante);
+				}
+			}
+		}
+		
+		return estudantes;
 	}
 	
 }
