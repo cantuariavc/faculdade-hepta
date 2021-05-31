364f20c97430b90eef8fd4f0d7332e5fd61ea1de
Vinícius Cantuária <cantuariavc@gmail.com>
2021-05-31 16:52:37 -0300

    WIP: Cria formulários

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -5,9 +5,9 @@ window.onload = () => {
 			var tableBody = document.getElementById('tableBody');
 			data.forEach(disciplina => {
 				// console.log(disciplina);
-				
+
 				var tr = document.createElement('tr');
-				
+
 				var id = document.createElement('td');
 				id.appendChild(document.createTextNode(disciplina.idDisciplina));
 				tr.appendChild(id);
@@ -15,7 +15,7 @@ window.onload = () => {
 				var nome = document.createElement('td');
 				nome.appendChild(document.createTextNode(disciplina.nome));
 				tr.appendChild(nome);
-				
+
 				var planoEnsino = document.createElement('td');
 				var planoEnsinoNome = document.createElement('a');
 				var nomeArquivo = `${disciplina.planoEnsinoNome}.pdf`;
@@ -24,14 +24,23 @@ window.onload = () => {
 				planoEnsinoNome.setAttribute('download', nomeArquivo);
 				planoEnsino.appendChild(planoEnsinoNome);
 				tr.appendChild(planoEnsino);
-				
-				var professor = document.createElement('td'); 
+
+				var professor = document.createElement('td');
 				professor.appendChild(document.createTextNode(disciplina.professorDTO.nomeCompleto));
 				tr.appendChild(professor);
-				
-				tr.appendChild(document.createElement('td'));
-				tr.appendChild(document.createElement('td'));
-				
+
+				var editarTD = document.createElement('td');
+				var editar = document.createElement('span');
+				editar.appendChild(document.createTextNode('editar'));
+				editarTD.appendChild(editar);
+				tr.appendChild(editarTD);
+
+				var excluirTD = document.createElement('td');
+				var excluir = document.createElement('span');
+				excluir.appendChild(document.createTextNode('excluir'));
+				excluirTD.appendChild(excluir);
+				tr.appendChild(excluirTD);
+
 				tableBody.appendChild(tr);
 			})
 		})
